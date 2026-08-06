d1df6d1b90607f7e8ad59ba4746f4a63a47f0f77
kairosachi-lab <kairosachi@gmail.com>
2026-08-06 14:32:20 +0100

    Updated product images

--- data/product.ts
+++ data/product.ts
@@ -3,16 +3,15 @@ export const products = [
     id: 1,
     name: "1 Litre Pure Honey",
     price: 12000,
-    image: "/images/big-honey.jpg",
+    image: "/images/honey 1.jpg",
     description:
       "100% pure raw honey with no additives or preservatives.",
   },
-
   {
     id: 2,
     name: "50cl Pure Honey",
     price: 6500,
-    image: "/images/small-honey.jpg",
+    image: "/images/honey 2.jpg",
     description:
       "Fresh natural honey harvested directly from trusted beekeepers.",
   },
